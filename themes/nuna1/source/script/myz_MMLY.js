66540a0b48caf4b65dbeb25455262c04fcdc15b7
durling <878877528@qq.com>
2017-10-21 18:51:41 +0800

    完善留言功能

--- themes/nuna1/source/script/myz_MMLY.js
+++ themes/nuna1/source/script/myz_MMLY.js
@@ -16,7 +16,7 @@
         		})
         	}
         	$(window).on('load',function(){
-        		$(".paginator").after('<div class="MMLY"><h4>给毛毛的留言：<a class="admin" target="_blank" href="http://h5.test.cloudm.com/n/news_myz/manage">管理</a></h4>'+htmlDOM+'<p style="text-align:center;"><button class="addLMsg-btn">添加留言</button></p></div>');
+        		$(".paginator").after('<div class="MMLY"><h4>给毛毛的精选留言：<a class="admin" target="_blank" href="http://h5.test.cloudm.com/n/news_myz/manage">管理</a></h4>'+htmlDOM+'<p style="text-align:center;"><button class="addLMsg-btn">添加留言</button></p></div>');
         	})
         },
         timeout:3000  
@@ -24,10 +24,35 @@
     $('main').on('click','.addLMsg-btn',function(){
 		// alert('暂未开放')
         $('body .MMLY-add').remove()
-        $('body').append('<div class="MMLY-add"><p><input placeholder="邮箱"/></p><p><input placeholder="留言内容"/></p><p><button class="MMLY-add-btn">提交</button></p></div>')
+        $('body').append('<div class="MMLY-add"><p><input class="title" placeholder="留言者用户名/邮箱"/></p><p><input class="content" placeholder="留言内容"/></p><p><button class="MMLY-ok-btn">提交</button></p><p><button class="MMLY-cancel-btn">取消</button></p></div>')
     })
-    $('body').on('click','.MMLY-add-btn',function(){
+    $('body').on('click','.MMLY-cancel-btn',function(){
         $('body .MMLY-add').remove()
+    })
+    $('body').on('click','.MMLY-ok-btn',function(){
+        var title=$('body').find('.MMLY-add input.title').val()
+        var content=$('body').find('.MMLY-add input.content').val()
+        if(!title||!content){
+            alert('请填写留言者和留言内容')
+            return
+        }
+        // alert('ajax')
+        $.ajax({  
+            url:"http://h5.test.cloudm.com/n/news_myz/api/addLeaveMsgJsonp?category=MM留言&title="+title+"&content="+content,
+            dataType:'jsonp',
+            data:'',
+            jsonp:'callback',  
+            success:function(result) {  
+                console.log(result)
+                if(result.head.retcode==0){
+                    alert('非常感谢，您的留言毛毛已收到！毛毛主人审核通过后会在留言列表展示')
+                    $('body .MMLY-add').remove()
+                }else{
+                    alert('留言失败（'+result.head.retmsg+'）')
+                }
+            },
+            timeout:3000  
+        });
     })
 }())
 
