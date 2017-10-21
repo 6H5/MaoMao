f56b949110d488afad8a6f2ff48e8997bddb2ead
durling <878877528@qq.com>
2017-10-21 21:42:10 +0800

    毛毛博客留言板

--- themes/nuna1/source/script/myz_MMLY.js
+++ themes/nuna1/source/script/myz_MMLY.js
@@ -2,25 +2,29 @@
 	function getLocalTime(nS) {     
 	   return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
 	}
-    $.ajax({
-        url:"http://h5.test.cloudm.com/n/news_myz/getdata?act=getNewsListJsonp&selectCategory=MM%E7%95%99%E8%A8%80&publishStatus=1",  
-        dataType:'jsonp',
-        data:'',
-        jsonp:'callback',  
-        success:function(result) {  
-        	console.log(result)
-        	var arr=result.data,htmlDOM=''
-        	if(arr.length>0){
-        		arr.forEach(function(n,i){
-        			htmlDOM+='<div class="ly-one"><p class="t"><span>'+getLocalTime(n.createtime)+'：</span><span style="float:right;"><span>来自'+n.title+'</span><span data-id='+n.id+' data-sort='+n.sort+' class="zan"><span>'+n.sort+'</span><img style="width:30px;" src="../zan-off.png"></span><span></p><p style="padding-right:100px;">'+n.content+'</p></div>'
-        		})
-        	}
-        	$(window).on('load',function(){
-        		$(".paginator").after('<div class="MMLY"><h4>给毛毛的留言：<a class="admin" target="_blank" href="http://h5.test.cloudm.com/n/news_myz/manage">管理</a></h4>'+htmlDOM+'<p style="text-align:center;"><button class="addLMsg-btn">添加留言</button></p></div>');
-        	})
-        },
-        timeout:3000  
-    });
+    function loadMMLY(){
+        // $('body .MMLY').remove()
+        $.ajax({
+            url:"http://h5.test.cloudm.com/n/news_myz/getdata?act=getNewsListJsonp&selectCategory=MM%E7%95%99%E8%A8%80&publishStatus=1",  
+            dataType:'jsonp',
+            data:'',
+            jsonp:'callback',  
+            success:function(result) {  
+                console.log(result)
+                var arr=result.data,htmlDOM=''
+                if(arr.length>0){
+                    arr.forEach(function(n,i){
+                        htmlDOM+='<div class="ly-one"><p class="t"><span>'+getLocalTime(n.createtime)+'：</span><span style="float:right;"><span>来自'+n.title+'</span><span data-id='+n.id+' data-sort='+n.sort+' class="zan"><span>'+n.sort+'</span><img style="width:30px;" src="../zan-off.png"></span><span></p><p style="padding-right:100px;">'+n.content+'</p></div>'
+                    })
+                }
+                $(window).on('load',function(){
+                    $(".paginator").after('<div class="MMLY"><h4>给毛毛的留言：<a class="admin" target="_blank" href="http://h5.test.cloudm.com/n/news_myz/manage">管理</a></h4>'+htmlDOM+'<p style="text-align:center;"><button class="addLMsg-btn">添加留言</button></p></div>');
+                })
+            },
+            timeout:3000  
+        });
+    }
+    loadMMLY()
     // 显示增加留言弹框
     $('main').on('click','.addLMsg-btn',function(){
 		// alert('暂未开放')
@@ -92,7 +96,6 @@
             },
             timeout:3000  
         });
-        
     })
 }())
 
